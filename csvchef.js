6bc2ef6f538096ef69875215218d0da4930805e9
Scott Olmsted <solmsted@gmail.com>
2016-09-30 13:31:34 -0400

    use double quoted string from string.ne

--- csvchef.js
+++ csvchef.js
@@ -2,6 +2,23 @@
 // http://github.com/Hardmath123/nearley
 (function () {
 function id(x) {return x[0]; }
+
+function nth(n) {
+    return function(d) {
+        return d[n];
+    };
+}
+
+
+function $(o) {
+    return function(d) {
+        var ret = {};
+        Object.keys(o).forEach(function(k) {
+            ret[k] = d[o[k]];
+        });
+        return ret;
+    };
+}
 var grammar = {
     ParserRules: [
     {"name": "_$ebnf$1", "symbols": []},
@@ -11,14 +28,44 @@ var grammar = {
     {"name": "__$ebnf$1", "symbols": ["wschar", "__$ebnf$1"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
     {"name": "__", "symbols": ["__$ebnf$1"], "postprocess": function(d) {return null;}},
     {"name": "wschar", "symbols": [/[ \t\n\v\f]/], "postprocess": id},
+    {"name": "dqstring$ebnf$1", "symbols": []},
+    {"name": "dqstring$ebnf$1", "symbols": ["dstrchar", "dqstring$ebnf$1"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
+    {"name": "dqstring", "symbols": [{"literal":"\""}, "dqstring$ebnf$1", {"literal":"\""}], "postprocess": function(d) {return d[1].join(""); }},
+    {"name": "sqstring$ebnf$1", "symbols": []},
+    {"name": "sqstring$ebnf$1", "symbols": ["sstrchar", "sqstring$ebnf$1"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
+    {"name": "sqstring", "symbols": [{"literal":"'"}, "sqstring$ebnf$1", {"literal":"'"}], "postprocess": function(d) {return d[1].join(""); }},
+    {"name": "btstring$ebnf$1", "symbols": []},
+    {"name": "btstring$ebnf$1", "symbols": [/[^`]/, "btstring$ebnf$1"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
+    {"name": "btstring", "symbols": [{"literal":"`"}, "btstring$ebnf$1", {"literal":"`"}], "postprocess": function(d) {return d[1].join(""); }},
+    {"name": "dstrchar", "symbols": [/[^\\"\n]/], "postprocess": id},
+    {"name": "dstrchar", "symbols": [{"literal":"\\"}, "strescape"], "postprocess": 
+        function(d) {
+            return JSON.parse("\""+d.join("")+"\"");
+        }
+        },
+    {"name": "sstrchar", "symbols": [/[^\\'\n]/], "postprocess": id},
+    {"name": "sstrchar", "symbols": [{"literal":"\\"}, "strescape"], "postprocess": 
+        function(d) {
+            return JSON.parse("\""+d.join("")+"\"");
+        }
+        },
+    {"name": "strescape", "symbols": [/["'\\/bfnrt]/], "postprocess": id},
+    {"name": "strescape", "symbols": [{"literal":"u"}, /[a-fA-F0-9]/, /[a-fA-F0-9]/, /[a-fA-F0-9]/, /[a-fA-F0-9]/], "postprocess": 
+        function(d) {
+            return d.join("");
+        }
+        },
     {"name": "operation$string$1", "symbols": [{"literal":"i"}, {"literal":"n"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "operation$string$2", "symbols": [{"literal":"r"}, {"literal":"e"}, {"literal":"p"}, {"literal":"l"}, {"literal":"a"}, {"literal":"c"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "operation$string$3", "symbols": [{"literal":"w"}, {"literal":"i"}, {"literal":"t"}, {"literal":"h"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "operation", "symbols": ["operation$string$1", "__", "field", "__", "operation$string$2", "__", "string_or_regexp", "__", "operation$string$3", "__", "string"]},
+    {"name": "operation", "symbols": ["operation$string$1", "__", "field", "__", "operation$string$2", "__", "string_or_regexp", "__", "operation$string$3", "__", "string"], "postprocess": 
+        function(d) {
+            return JSON.parse(["\"", d[2], ":str.replace(", d[6], ",@@@", d[10], "@@@)\""].join("")).replace(/@@@/g, "\"");
+        }
+        },
     {"name": "string_or_regexp", "symbols": ["string"]},
     {"name": "string_or_regexp", "symbols": ["regexp"]},
-    {"name": "string$string$1", "symbols": [{"literal":"'"}, {"literal":"x"}, {"literal":"y"}, {"literal":"z"}, {"literal":"'"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "string", "symbols": ["string$string$1"]},
+    {"name": "string", "symbols": ["dqstring"]},
     {"name": "field$string$1", "symbols": [{"literal":"N"}, {"literal":"a"}, {"literal":"m"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "field", "symbols": ["field$string$1"]},
     {"name": "regexp$string$1", "symbols": [{"literal":"/"}, {"literal":"a"}, {"literal":"b"}, {"literal":"c"}, {"literal":"/"}], "postprocess": function joiner(d) {return d.join('');}},
--- csvchef.ne
+++ csvchef.ne
@@ -1,14 +1,20 @@
 # CSV Chef
 
-#@builtin "string.ne"
 @builtin "whitespace.ne"
+@builtin "string.ne"
 
-operation -> "in" __ field __ "replace" __ string_or_regexp __ "with" __ string
+operation -> "in" __ field __ "replace" __ string_or_regexp __ "with" __ string {%
+    function(d) {
+        return JSON.parse(["\"", d[2], ":str.replace(", d[6], ",@?^", d[10], "@?^)\""].join("")).replace(/@@@/g, "\"");
+    }
+%}
 
-#operation -> field __ "replace" __ string_or_regexp
+#  parse("\""+d[2]+":replace.("+d[6]+",\""+d[10]+"\"\"");   [ 'in::Name::replace::/abc/::with::xyz' ]   str.replace("Microsoft", "W3Schools");
+
+#operation -> field __ "replace" __ string_or_regexp   
 
 string_or_regexp -> string | regexp
 
-string -> "'xyz'"   # sqstring
+string ->  dqstring  # "'xyz'"   # 
 field  -> "Name"    # string
 regexp -> "/abc/"   # "/" string "/"
