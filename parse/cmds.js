dbd428e49a942cf9c5086070d0ef400f866d0683
Scott Olmsted <solmsted@gmail.com>
2016-10-13 15:38:34 -0400

    Handle rows with operation in each in cmds.ne

--- parse/cmds.js
+++ parse/cmds.js
@@ -3,17 +3,34 @@
 (function () {
 function id(x) {return x[0]; }
 
-  function flatten(arr) {
-    return arr.reduce(function (flat, toFlatten) {
-      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
-    }, []);
-  }
+    function depth( a ) {
+        if (a.constructor === Array) {
+            return 1 + depth(a[0]);
+        } else {
+            return 0;
+        }
+    }
+    
+    function remove_nesting( d ) {
+        if (d.constructor === Array & d.length === 1) {
+            return remove_nesting(d[0]);
+        } else {
+            return d;
+        }
+    }
 
-  function command_write(d) {
-    colname = d[2][0];
-    string = d[6][0];
-    return ["WRITE", colname, string];
-  }
+    function operation( op, d ) {
+        switch(op) {
+            case "WRITE":
+                colname = d[2][0];
+                string = d[6][0];
+                return ["WRITE", colname, string];
+                break;
+            default:
+                return ["UNKNOWN"];
+        }
+
+    }
 
 
 var appendItem = function (a, b) { return function (d) { return d[a].concat([d[b]]); } };
@@ -22,12 +39,12 @@ var empty = function (d) { return []; };
 var emptyStr = function (d) { return ""; };
 var grammar = {
     ParserRules: [
-    {"name": "cmds", "symbols": []},
-    {"name": "cmds", "symbols": ["cmd"]},
-    {"name": "cmds", "symbols": ["cmd", "newline", "cmds"]},
-    {"name": "cmd$string$1", "symbols": [{"literal":"t"}, {"literal":"o"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "cmd$string$2", "symbols": [{"literal":"w"}, {"literal":"r"}, {"literal":"i"}, {"literal":"t"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "cmd", "symbols": ["cmd$string$1", "__", "string", "__", "cmd$string$2", "__", "string"], "postprocess": function (d) { return command_write(d); }},
+    {"name": "rows", "symbols": []},
+    {"name": "rows", "symbols": ["row"]},
+    {"name": "rows", "symbols": ["rows", "newline", "row"], "postprocess": appendItem(0,2)},
+    {"name": "row$string$1", "symbols": [{"literal":"t"}, {"literal":"o"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "row$string$2", "symbols": [{"literal":"w"}, {"literal":"r"}, {"literal":"i"}, {"literal":"t"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "row", "symbols": ["row$string$1", "__", "string", "__", "row$string$2", "__", "string"], "postprocess": function (d) { return operation("WRITE", d); }},
     {"name": "string", "symbols": ["quoted_string"]},
     {"name": "string", "symbols": ["unquoted_string"]},
     {"name": "quoted_string$ebnf$1", "symbols": []},
@@ -50,7 +67,7 @@ var grammar = {
     {"name": "__", "symbols": ["__$ebnf$1"], "postprocess": function(d) {return null;}},
     {"name": "wschar", "symbols": [/[ \t\v\f]/], "postprocess": id}
 ]
-  , ParserStart: "cmds"
+  , ParserStart: "rows"
 }
 if (typeof module !== 'undefined'&& typeof module.exports !== 'undefined') {
    module.exports = grammar;
--- parse/cmds.ne
+++ parse/cmds.ne
@@ -1,41 +1,33 @@
 
 @{%
-  function flatten(arr) {
-    return arr.reduce(function (flat, toFlatten) {
-      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
-    }, []);
-  }
+    function depth( a ) {
+        if (a.constructor === Array) {
+            return 1 + depth(a[0]);
+        } else {
+            return 0;
+        }
+    }
+    
+    function remove_nesting( d ) {
+        if (d.constructor === Array & d.length === 1) {
+            return remove_nesting(d[0]);
+        } else {
+            return d;
+        }
+    }
 
-NOT WORKING - EXTRACT COMMANDS FROM VARYING DEPTHS IN nearley result
+    function operation( op, d ) {
+        switch(op) {
+            case "WRITE":
+                colname = d[2][0];
+                string = d[6][0];
+                return ["WRITE", colname, string];
+                break;
+            default:
+                return ["UNKNOWN"];
+        }
 
-  // assumes arg is array
-  function extract_element(a) {
-      switch(a.length) {
-      case 0:  // null or []
-          return null;
-          break;
-      case 1:  // nested [ ]
-          return extract_element(a[0])
-          break;
-      default: // command
-          if (a[0].class != Array) {
-            return a;
-          } else {
-            return a.map(extract_elements);
-          }
-      }
-  }
-
-  function extract_commands(d) {
-    dd = d.map(extract_element);
-    return dd.filter(function(e){ return e.length != 0 });
-  }
-
-  function command_write(d) {
-    colname = d[2][0];
-    string = d[6][0];
-    return ["WRITE", colname, string];
-  }
+    }
 %}
 
 @{%
@@ -45,13 +37,13 @@ var empty = function (d) { return []; };
 var emptyStr = function (d) { return ""; };
 %}
 
-cmds              -> null
-                   | cmd
-                   | cmd newline cmds                 #{% function(d) { return flatten(d); } %}
+rows              -> null
+                   | row
+                   | rows newline row                                       {% appendItem(0,2) %}
 
-cmd               -> "to" __ string __ "write" __ string    {% function (d) { return command_write(d); } %}
+row               -> "to" __ string __ "write" __ string                    {% function (d) { return operation("WRITE", d); } %}
 
-string             -> quoted_string                   #{% id %} # {% function (d) { console.log(JSON.stringify(d[0], null, 4)); return d[0].join(""); } %}
+string             -> quoted_string
                    | unquoted_string
 
 quoted_string      -> quote quoted_char:* quote       {% function (d) { return d[1].join(""); } %}
